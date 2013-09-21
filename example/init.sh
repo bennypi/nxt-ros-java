5d23dcf85f472164483273105ef2aaf558b75f3f
sanic <sanic@asgard>
2013-09-21 17:34:53 +0200

    added example project

---
+++ example/init.sh
@@ -0,0 +1,5 @@
+PATH_TO_GRADLEW=../../rosjava_core
+eval "$PATH_TO_GRADLEW/gradlew clean"
+eval "$PATH_TO_GRADLEW/gradlew build"
+eval "$PATH_TO_GRADLEW/gradlew eclipse"
+
---
+++ nxt_ros_java/buildJar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd bin
+jar cf ../nxt_java_handler.jar org/ros/nxt_ros_java/Talker* org/ros/nxt_ros_java/NxtJavaHandler.class
